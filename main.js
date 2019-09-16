ec543bdbbfb5776b25e108f46d60e00a1fde4224
Garrick Northover <garricknorthover@gmail.com>
2019-09-17 10:07:55 +1200

    added cardvalues

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const suit = {hearts: "Hearts", spades: "Spades", diamonds: "Diamonds", clubs: "Clubs"}
+const Suit = { hearts: "Hearts", spades: "Spades", diamonds: "Diamonds", clubs: "Clubs" }
+const CardValue = {
+    one: "One",
+    two: "Two",
+    three: "Three",
+    foor: "Four",
+    five: "Five",
+    six: "Six",
+    seven: "Seven",
+    eight: "Eight",
+    nine: "Nine",
+    ten: "Ten",
+    jack: "Jack",
+    queen: "Queen",
+    king: "King",
+    ace: "Ace"
+}
