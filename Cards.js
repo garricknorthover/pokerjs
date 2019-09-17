8b0f8a71f541d5f8137474a3f2cb673e914c6a66
Garrick Northover <garricknorthover@gmail.com>
2019-09-17 14:19:24 +1200

    making sure it pushed

---
+++ Cards.js
@@ -0,0 +1,17 @@
+const Suit = { hearts: "Hearts", spades: "Spades", diamonds: "Diamonds", clubs: "Clubs" }
+const CardValue = {
+    one: "One",
+    two: "Two",
+    three: "Three",
+    four: "Four",
+    five: "Five",
+    six: "Six",
+    seven: "Seven",
+    eight: "Eight",
+    nine: "Nine",
+    ten: "Ten",
+    jack: "Jack",
+    queen: "Queen",
+    king: "King",
+    ace: "Ace"
+}
---
+++ main.test.js
@@ -0,0 +1,7 @@
+const {test} = require('jest')
+const {Card, Suit, CardValue} = require('./main')
+
+// test('converts cards to words', () =>{
+//     expect(Card(ace,clubs).ToBe("Ace of Clubs"))
+// })
+Card(ace,clubs)
