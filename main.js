d444f6e8dd8e28c8f568fc4c99ea73a6c4734ace
Garrick Northover <garricknorthover@gmail.com>
2019-09-17 14:18:47 +1200

    before refactor after being able to get random card

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const CardValue = {
     one: "One",
     two: "Two",
     three: "Three",
-    foor: "Four",
+    four: "Four",
     five: "Five",
     six: "Six",
     seven: "Seven",
@@ -15,4 +15,18 @@ const CardValue = {
     king: "King",
     ace: "Ace"
 }
+const Card = (value, suit) => `${value} of ${suit}`
+const SuitKeys = Object.keys(Suit)
+const ValueKeys = Object.keys(CardValue)
+SuitKeys
+ValueKeys
 
+const RandomSuitKey = SuitKeys[Math.random() * 4 << 0]
+const RandomValueKey = ValueKeys[Math.random() * 14 << 0]
+
+RandomSuitKey
+RandomValueKey
+
+const RandomCard = () => Card(RandomValueKey, RandomSuitKey)
+const RandomDraw = RandomCard()
+RandomDraw
