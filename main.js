7204c6b76c413eab142a3c1f1b283cff93f2a07a
Garrick Northover <garricknorthover@gmail.com>
2019-09-18 22:23:14 +1200

    babel works

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const {CardValue, Suit} = require('./Cards')
+import {CardValue, Suit} from './Cards'
 
 const RandomSuit = Object.values(Suit)[Math.random() * 4 << 0]
 const RandomCardValue = Object.values(CardValue)[Math.random() * 14 << 0]
 
-const RandomCard =`${RandomCardValue} of ${RandomSuit}`
+const Card = (value, suit) => `${value} of ${suit}`
+
+const RandomCard = Card(RandomCardValue, RandomSuit)
 
 console.log(RandomCard)
 
