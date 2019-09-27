59dd4372738aa46f91bb98c695ac414921ef1913
Garrick Northover <garricknorthover@gmail.com>
2019-09-27 19:58:52 +1200

    card values to totitle is sometimes undefined

--- scratchPad.js
+++ scratchPad.js
@@ -3,9 +3,11 @@ const Suit = { Hearts: 1, Spades: 2, Diamonds: 3, Clubs: 4 }
 const CardValue = {
     Two: 2, Three: 3, Four: 4, Five: 5, Six: 6, Seven: 7, Eight: 8, Nine: 9, Ten: 10, Jack: 11, Queen: 12, King: 13, Ace: 14
 }
+const twotofourteen = () => 2 + Math.random() * 13 << 0
 
 const randomSuit = () => Object.keys(Suit)[Math.random() * 4 << 0]
-const randomCardValue = () => Object.keys(CardValue)[2 + Math.random() * 13 << 0]
+const randomCardValue = () => {
+    return Object.keys(CardValue)[twotofourteen()] }
 
 const drawCard = (value, suit) => ({ CardValue: value, Suit: suit })
 
