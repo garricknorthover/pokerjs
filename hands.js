75191b36a6ee83c45b67ca4ab915fb1f9d05afa9
Garrick Northover <garricknorthover@gmail.com>
2019-10-16 21:25:37 +1300

    tidyup

--- hands.js
+++ hands.js
@@ -3,14 +3,23 @@ import { toObjValues, ofSameKind } from './util'
 
 
 const isFlush = (f) => f.every(x => x.suit == f[0].suit)
+
 const isAStraight = (ob) => toObjValues(ob).every((x, indx, arr) => indx == arr.length - 1 ? x == arr[indx - 1] + 1 : x == arr[indx + 1] - 1)
+
 const findHighcard = (ob) => Object.keys(cardValueEnum)[toObjValues(ob).slice(-1) - 2]
+
 const isStraightFlush = (ob) => isFlush(ob) && isAStraight(ob)
+
 const isRoyalFlush = (ob) => isStraightFlush(ob) && findHighcard(ob) === 'Ace'
+
 const isFourKind = (obj) => ofSameKind(obj)[0] === 4
+
 const isThreeKind = (obj) => ofSameKind(obj)[0] === 3 && ofSameKind(obj)[1] !== 2
+
 const isTwoPair = (obj) => (ofSameKind(obj)[0] && ofSameKind(obj)[1]) === 2
+
 const isFullHouse = (obj) => ofSameKind(obj)[0] === 3 && ofSameKind(obj)[1] === 2
+
 const isPair = (obj) => ofSameKind(obj)[0] === 2 && ofSameKind(obj)[1] === 1
 
 
--- util.js
+++ util.js
@@ -2,7 +2,6 @@ import { cardValueEnum, suitEnum } from './cards'
 
 const toObjValues = (ob) => ob.map(x => x.card).map(v => cardValueEnum[v]).sort((a, b) => a - b)
 
-
 const ofSameKind = (cards) => {
     const theSet = toObjValues(cards)
     return [... new Set(theSet)].map(x => theSet.filter((y) => x == y).length).reverse()
@@ -13,9 +12,7 @@ let deck = Object.keys(suitEnum)
     .flat()
     .sort((a, b) => 0.5 - Math.random())// quick and dirty random shuffle
 
-const hand = () => deck.splice(0,5)
-
-
+const hand = () => deck.splice(0, 5)
 
 
 export { toObjValues, ofSameKind, deck, hand }
